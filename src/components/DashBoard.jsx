efe06466662a44d262b5b22b5a73cacb59f1fe6d
Suresh Konar <SKonar@evolenthealth.com>
2023-01-01 21:01:01 +0530

    dashboard form implemented using autocomplete

--- src/components/DashBoard.jsx
+++ src/components/DashBoard.jsx
@@ -1,57 +1,43 @@
-import { useState } from 'react';
 import { useDispatch } from 'react-redux';
-import { useForm } from 'react-hook-form';
+import { useForm, Controller } from 'react-hook-form';
 import Button from '@mui/material/Button';
 import Card from '@mui/material/Card';
 import Grid from '@mui/material/Grid';
 import TextField from '@mui/material/TextField';
 import Typography from '@mui/material/Typography';
-import MenuItem from '@mui/material/MenuItem';
+import { isEmpty } from 'lodash';
+import Autocomplete from '@mui/material/Autocomplete';
 // services
 import { saveExamData } from '../ducks/examInfo';
+import { EXAM_CATEGORY, EXAM_RESERVATION, EXAM_ZONES } from '../constants/exam';
+import { ERROR_MESSAGE } from '../constants';
 
 function DashBoard() {
   const dispatch = useDispatch();
   const {
-    register,
     handleSubmit,
     reset,
     formState: { errors },
-  } = useForm();
-
-  const [category, setCategory] = useState('');
-  const [reservation, setReservation] = useState('');
-  const [zone, setZone] = useState('');
+    control,
+  } = useForm({
+    mode: 'onChange',
+    defaultValues: { zone: '', reservation: '', category: '', examUrl: '' },
+  });
 
   const onSubmit = (data) => {
     console.info('data ', data);
   };
 
-  const handleSaveData = (data) => {
-    console.info(dispatch(saveExamData(data)));
-  };
+  // const handleSaveData = (data) => {
+  //   console.info(dispatch(saveExamData(data)));
+  // };
 
   const handleReset = () => {
-    setCategory('');
-    setReservation('');
-    setZone('');
     reset();
   };
 
   return (
     <div>
-      {/* <div style={{ paddingTop: '5rem' }}>
-        <Button
-          type="button"
-          onClick={() => {
-            handleSaveData({
-              url: 'https://dc4-g22.digialm.com//per/g22/pub/32341/touchstone/AssessmentQPHTMLMode1//32341O2230/32341O2230S24D23621/16628815322939738/124194210189066_32341O2230S24D23621E1.html',
-            });
-          }}
-        >
-          Click for Checking Result
-        </Button>
-      </div> */}
       <Grid
         container
         spacing={{ xs: 2, md: 3 }}
@@ -82,104 +68,144 @@ function DashBoard() {
             <form>
               <Grid container spacing={2}>
                 <Grid item xs={12}>
-                  <TextField
-                    id="examUrl"
-                    name="Answersheet Url"
-                    label="Answersheet Url"
-                    defaultValue=""
-                    fullWidth
-                    {...register('examUrl', {
-                      required: 'Please enter a valid response key url',
-                    })}
-                    helperText="Enter your response key URL here"
-                    error={Boolean(errors.examUrl)}
+                  <Controller
+                    name="examUrl"
+                    control={control}
+                    rules={{ required: ERROR_MESSAGE.REQUIRED }}
+                    render={({
+                      field: { ref, ...urlProps },
+                      fieldState: { invalid, error },
+                    }) => (
+                      <TextField
+                        label="Answersheet Url"
+                        fullWidth
+                        error={invalid}
+                        helperText={error?.message}
+                        {...urlProps}
+                      />
+                    )}
                   />
-                  <Typography variant="inherit" color="textSecondary">
-                    {errors.examUrl?.message}
-                  </Typography>
                 </Grid>
                 <Grid item xs={12}>
-                  <TextField
-                    select
-                    id="category-dropdown"
+                  <Controller
+                    control={control}
                     name="category"
-                    label="Select category"
-                    fullWidth
-                    value={category}
-                    defaultValue=""
-                    {...register('category', {
-                      onChange: (event) => {
-                        setCategory(event.target.value);
-                      },
-                      required: 'Please select category',
-                    })}
-                    error={Boolean(errors.category)}
-                  >
-                    <MenuItem value="open">Open</MenuItem>
-                    <MenuItem value="obc">OBC</MenuItem>
-                    <MenuItem value="ews">EWS</MenuItem>
-                  </TextField>
-                  <Typography variant="inherit" color="textSecondary">
-                    {errors.category?.message}
-                  </Typography>
+                    rules={{ required: ERROR_MESSAGE.REQUIRED }}
+                    render={({
+                      field: { onChange, value },
+                      fieldState: { error, invalid },
+                    }) => (
+                      <Autocomplete
+                        onChange={(event, item) => {
+                          onChange(item);
+                        }}
+                        value={value}
+                        options={EXAM_CATEGORY}
+                        getOptionLabel={(item) =>
+                          item.label ? item.label : ''
+                        }
+                        getOptionSelected={(option, val) =>
+                          val === undefined ||
+                          val === '' ||
+                          option.value === val.value
+                        }
+                        renderInput={(params) => (
+                          <TextField
+                            {...params}
+                            label="Category"
+                            margin="normal"
+                            variant="outlined"
+                            error={invalid}
+                            helperText={error?.message}
+                            required
+                          />
+                        )}
+                      />
+                    )}
+                  />
                 </Grid>
                 <Grid item xs={12}>
-                  <TextField
-                    select
-                    id="reservation-dropdown"
+                  <Controller
+                    control={control}
                     name="reservation"
-                    label="Select reservation"
-                    fullWidth
-                    value={reservation}
-                    defaultValue=""
-                    onChange={(e) => console.log(e.target.value)}
-                    {...register('reservation', {
-                      onChange: (event) => {
-                        setReservation(event.target.value);
-                      },
-                      required: 'Please select reservation',
-                    })}
-                    error={Boolean(errors.reservation)}
-                  >
-                    <MenuItem value="open">Open</MenuItem>
-                    <MenuItem value="obc">OBC</MenuItem>
-                    <MenuItem value="ews">EWS</MenuItem>
-                  </TextField>
-                  <Typography variant="inherit" color="textSecondary">
-                    {errors.reservation?.message}
-                  </Typography>
+                    rules={{ required: ERROR_MESSAGE.REQUIRED }}
+                    render={({
+                      field: { onChange, value },
+                      fieldState: { error, invalid },
+                    }) => (
+                      <Autocomplete
+                        onChange={(event, item) => {
+                          onChange(item);
+                        }}
+                        value={value}
+                        options={EXAM_RESERVATION}
+                        getOptionLabel={(item) =>
+                          item.label ? item.label : ''
+                        }
+                        getOptionSelected={(option, val) =>
+                          val === undefined ||
+                          val === '' ||
+                          option.value === val.value
+                        }
+                        renderInput={(params) => (
+                          <TextField
+                            {...params}
+                            label="Reservation"
+                            margin="normal"
+                            variant="outlined"
+                            error={invalid}
+                            helperText={error?.message}
+                            required
+                          />
+                        )}
+                      />
+                    )}
+                  />
                 </Grid>
                 <Grid item xs={12}>
-                  <TextField
-                    select
-                    id="zone-dropdown"
-                    name="Zone"
-                    label="Select zone"
-                    fullWidth
-                    value={zone}
-                    defaultValue=""
-                    {...register('zone', {
-                      onChange: (event) => {
-                        setZone(event.target.value);
-                      },
-                      required: 'Please select Zone',
-                    })}
-                    error={Boolean(errors.zone)}
-                  >
-                    <MenuItem value="open">East</MenuItem>
-                    <MenuItem value="obc">West</MenuItem>
-                    <MenuItem value="ews">North</MenuItem>
-                    <MenuItem value="ews">South</MenuItem>
-                  </TextField>
-                  <Typography variant="inherit" color="textSecondary">
-                    {errors.zone?.message}
-                  </Typography>
+                  <Controller
+                    control={control}
+                    name="zone"
+                    rules={{ required: ERROR_MESSAGE.REQUIRED }}
+                    render={({
+                      field: { onChange, value },
+                      fieldState: { error, invalid },
+                    }) => (
+                      <Autocomplete
+                        onChange={(event, item) => {
+                          onChange(item);
+                        }}
+                        value={value}
+                        options={EXAM_ZONES}
+                        getOptionLabel={(item) =>
+                          item.label ? item.label : ''
+                        }
+                        getOptionSelected={(option, val) =>
+                          val === undefined ||
+                          val === '' ||
+                          option.value === val.value
+                        }
+                        renderInput={(params) => (
+                          <TextField
+                            {...params}
+                            label="Zone"
+                            margin="normal"
+                            variant="outlined"
+                            error={invalid}
+                            helperText={error?.message}
+                            required
+                          />
+                        )}
+                      />
+                    )}
+                  />
                 </Grid>
                 <Grid item xs={6}>
                   <Button
                     size="large"
                     variant="contained"
                     fullWidth
+                    disabled={!isEmpty(errors)}
                     onClick={handleSubmit(onSubmit)}
                   >
                     Submit
